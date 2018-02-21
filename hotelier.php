40ac48010677021f9f51b2784b7ef0b5a0428a0c
Benito Lopez <hello@lopezb.com>
2018-02-21 02:11:30 +0100

    Bum version

--- hotelier.php
+++ hotelier.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Easy WP Hotelier
  * Plugin URI:        https://wphotelier.com/?utm_source=wpadmin&utm_medium=plugin&utm_campaign=wphotelierplugin
  * Description:       Manage your hotel bookings with WordPress.
- * Version:           1.3.0
+ * Version:           1.4.0
  * Author:            Easy WP Hotelier
  * Author URI:        https://wphotelier.com/
  * Requires at least: 4.0
@@ -32,7 +32,7 @@ final class Hotelier {
 	/**
 	 * @var string
 	 */
-	public $version = '1.3.0';
+	public $version = '1.4.0';
 
 	/**
 	 * @var Hotelier The single instance of the class
