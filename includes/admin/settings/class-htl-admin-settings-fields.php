f3c8e03fe81b48f14e198813acec63eda695c02b
Benito Lopez <hello@lopezb.com>
2018-11-13 16:11:19 +0100

    Allow more intervals for checkin/checkout hotel hours

--- includes/admin/settings/class-htl-admin-settings-fields.php
+++ includes/admin/settings/class-htl-admin-settings-fields.php
@@ -5,7 +5,7 @@
  * @author   Benito Lopez <hello@lopezb.com>
  * @category Admin
  * @package  Hotelier/Admin
- * @version  1.7.0
+ * @version  1.8.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -415,32 +415,12 @@ class HTL_Admin_Settings_Fields {
 		$from = $value[ 'from' ];
 		$to   = $value[ 'to' ];
 
-		$options = array(
-			'0'  => '00:00',
-			'1'  => '01:00',
-			'2'  => '02:00',
-			'3'  => '03:00',
-			'4'  => '04:00',
-			'5'  => '05:00 ',
-			'6'  => '06:00',
-			'7'  => '07:00',
-			'8'  => '08:00',
-			'9'  => '09:00',
-			'10' => '10:00',
-			'11' => '11:00',
-			'12' => '12:00',
-			'13' => '13:00',
-			'14' => '14:00',
-			'15' => '15:00',
-			'16' => '16:00',
-			'17' => '17:00',
-			'18' => '18:00',
-			'19' => '19:00',
-			'20' => '20:00',
-			'21' => '21:00',
-			'22' => '22:00',
-			'23' => '23:00'
-		);
+		$options = array();
+
+		for ( $i = 0; $i <= 24; $i++ ) {
+			$options[ $i ] = sprintf( '%02d', $i ) . ':00';
+			$options[ $i + 25 ] = sprintf( '%02d', $i ) . ':30';
+		}
 
 		$html = '<label class="from-to">' . esc_html__( 'From:', 'wp-hotelier' ) . '<select name="hotelier_settings[' . esc_attr( $args[ 'id' ] ) . '][from]">';
 
