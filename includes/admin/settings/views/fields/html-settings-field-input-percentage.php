c17bbe48879b6a1e049aff3504b39f9977c2ebbc
Benito Lopez <hello@lopezb.com>
2019-03-07 16:34:22 +0100

    Remove for attribute when no necessary

--- includes/admin/settings/views/fields/html-settings-field-input-percentage.php
+++ includes/admin/settings/views/fields/html-settings-field-input-percentage.php
@@ -19,5 +19,5 @@ $placeholder = isset( $args[ 'placeholder' ] ) ? $args[ 'placeholder' ] : '';
 <div class="htl-ui-setting htl-ui-setting--percentage htl-ui-setting--<?php echo esc_attr( $args[ 'id' ] ); ?>">
 	<input type="text" class="htl-ui-input htl-ui-input--percentage" id="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>]" name="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>]" value="<?php echo esc_attr( $value ); ?>" placeholder="<?php echo esc_attr( $placeholder ); ?>" />
 
-	<label class="htl-ui-label htl-ui-label--text htl-ui-setting__description htl-ui-setting__description--percentage htl-ui-setting__description--<?php echo esc_attr( $args[ 'id' ] ); ?>" for="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>]"><?php echo wp_kses_post( $args[ 'desc' ] ); ?></label>
+	<label class="htl-ui-label htl-ui-label--text htl-ui-setting__description htl-ui-setting__description--percentage htl-ui-setting__description--<?php echo esc_attr( $args[ 'id' ] ); ?>"><?php echo wp_kses_post( $args[ 'desc' ] ); ?></label>
 </div>
--- includes/admin/settings/views/fields/html-settings-field-input-textarea.php
+++ includes/admin/settings/views/fields/html-settings-field-input-textarea.php
@@ -20,5 +20,5 @@ $size        = ( isset( $args[ 'size' ] ) && ! is_null( $args[ 'size' ] ) ) ? 'h
 <div class="htl-ui-setting htl-ui-setting--textarea htl-ui-setting--<?php echo esc_attr( $args[ 'id' ] ); ?>">
 	<textarea class="<?php echo esc_attr( $size ); ?> htl-ui-input htl-ui-input--textarea" id="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>]" name="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>]" placeholder="<?php echo esc_attr( $placeholder ); ?>"><?php echo esc_attr( $value ); ?></textarea>
 
-	<label for="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>]" class="htl-ui-label htl-ui-label--textarea htl-ui-setting__description htl-ui-setting__description--textarea htl-ui-setting__description--<?php echo esc_attr( $args[ 'id' ] ); ?>"><?php echo wp_kses_post( $args[ 'desc' ] ); ?></label>
+	<label class="htl-ui-label htl-ui-label--textarea htl-ui-setting__description htl-ui-setting__description--textarea htl-ui-setting__description--<?php echo esc_attr( $args[ 'id' ] ); ?>"><?php echo wp_kses_post( $args[ 'desc' ] ); ?></label>
 </div>
