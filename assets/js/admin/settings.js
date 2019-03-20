dc109f130c2fe310bfbcb86fe1f303930ae98997
Benito Lopez <hello@lopezb.com>
2019-03-20 17:37:49 +0100

    Remove old JS code

--- assets/js/admin/settings.js
+++ assets/js/admin/settings.js
@@ -6,9 +6,6 @@ jQuery(function ($) {
 	var HTL_Settings = {
 		init: function () {
 			this.show_uploader();
-			this.show_unforce_ssl();
-			this.show_pets_message();
-			this.show_book_now_quantity();
 			this.seasonal_dates_datepicker();
 			this.add_seasonal_rule();
 		},
@@ -55,69 +52,6 @@ jQuery(function ($) {
 
 				// Finally, open the modal.
 				file_frame.open();
-			});
-		},
-
-		show_unforce_ssl: function () {
-			var force_ssl_input = $('input[name="hotelier_settings[enforce_ssl_booking]"]');
-			var unforce_ssl_field = $('#hotelier_settings\\[unforce_ssl_booking\\]').closest('tr');
-
-			unforce_ssl_field.hide();
-
-			if (force_ssl_input.is(':checked')) {
-				unforce_ssl_field.show();
-			}
-
-			force_ssl_input.on('click', function () {
-				var _this = $(this);
-
-				if (_this.is(':checked')) {
-					unforce_ssl_field.show();
-				} else {
-					unforce_ssl_field.hide();
-				}
-			});
-		},
-
-		show_pets_message: function () {
-			var pets_input = $('input[name="hotelier_settings[hotel_pets]"]');
-			var pets_message = $('#hotelier_settings\\[hotel_pets_message\\]').closest('tr');
-
-			pets_message.hide();
-
-			if (pets_input.is(':checked')) {
-				pets_message.show();
-			}
-
-			pets_input.on('click', function () {
-				var _this = $(this);
-
-				if (_this.is(':checked')) {
-					pets_message.show();
-				} else {
-					pets_message.hide();
-				}
-			});
-		},
-
-		show_book_now_quantity: function () {
-			var book_now_behaviour = $('input[name="hotelier_settings[book_now_redirect_to_booking_page]"]');
-			var book_now_quantity = $('#hotelier_settings\\[book_now_allow_quantity_selection\\]').closest('tr');
-
-			book_now_quantity.hide();
-
-			if (book_now_behaviour.is(':checked')) {
-				book_now_quantity.show();
-			}
-
-			book_now_behaviour.on('click', function () {
-				var _this = $(this);
-
-				if (_this.is(':checked')) {
-					book_now_quantity.show();
-				} else {
-					book_now_quantity.hide();
-				}
 			});
 		},
 
