65f0ba7a79d45602253cd5c43bcb57d8c9f8bd7c
Benito Lopez <hello@lopezb.com>
2019-04-05 15:58:29 +0200

    Minor fix in header settings

--- includes/admin/settings/views/html-settings-header.php
+++ includes/admin/settings/views/html-settings-header.php
@@ -10,21 +10,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="hotelier-settings-header">
-	<div class="hotelier-settings-header__brand">
-		<h3 class="hotelier-settings-header__brand--title"><?php esc_attr_e( 'Easy WP Hotelier', 'wp-hotelier' ); ?></h3>
-	</div>
-
 	<a href="<?php echo esc_url( admin_url( 'admin.php?page=hotelier-calendar' ) ) ?>" class="htl-ui-button htl-ui-button--view-calendar"><?php esc_attr_e( 'View calendar', 'wp-hotelier' ); ?></a>
 
 	<span class="hotelier-settings-header__version-number"><strong><?php esc_attr_e( 'Easy WP Hotelier', 'wp-hotelier' ); ?></strong><?php echo sprintf( esc_html__( 'Version %s', 'wp-hotelier' ), HTL_VERSION ); ?></span>
 
 	<?php $is_hotelier_pro_installed = class_exists( 'HotelierPro' ) ? true : false; ?>
 
-	<span class="hotelier-settings-header__version-info <?php echo $is_hotelier_pro_installed ? 'hotelier-settings-header__version-info--pro' : ''; ?>">
+	<span class="hotelier-settings-header__version-info <?php echo $is_hotelier_pro_installed ? 'hotelier-settings-header__version-info--pro-version' : 'hotelier-settings-header__version-info--free-version'; ?>">
 		<?php if ( $is_hotelier_pro_installed ) : ?>
 			<?php esc_attr_e( 'Pro', 'wp-hotelier' ); ?>
 		<?php else : ?>
-			<a href="https://wphotelier.com/"><?php esc_attr_e( 'Upgrade to pro version', 'wp-hotelier' ); ?></a>
+			<a href="https://wphotelier.com/"><?php esc_attr_e( 'Upgrade to Pro version', 'wp-hotelier' ); ?></a>
 		<?php endif; ?>
 	</span>
 </div>
