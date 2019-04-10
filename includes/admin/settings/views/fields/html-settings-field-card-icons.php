4aa0957f32c7e88f45ed138d1a0dd9197c4208dc
Benito Lopez <hello@lopezb.com>
2019-04-10 16:23:37 +0200

    Fixes on card icons setting

--- includes/admin/settings/views/fields/html-settings-field-card-icons.php
+++ includes/admin/settings/views/fields/html-settings-field-card-icons.php
@@ -14,12 +14,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="htl-ui-setting htl-ui-setting--card-icons htl-ui-setting--<?php echo esc_attr( $args[ 'id' ] ); ?>">
 	<?php foreach ( $args[ 'options' ] as $key => $option ) : ?>
 		<?php $enabled = isset( $this->options[ $args[ 'id' ] ][ $key ] ) ? 1 : NULL; ?>
+		<div class="htl-ui-card-icon-wrapper">
+			<input name="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>][<?php echo esc_attr( $key ); ?>]" id="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>][<?php echo esc_attr( $key ); ?>]" class="htl-ui-input htl-ui-input--checkbox htl-ui-input--card-icon" type="checkbox" value="1" <?php echo checked( 1, $enabled, false ); ?> />
 
-		<input name="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>][<?php echo esc_attr( $key ); ?>]" id="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>][<?php echo esc_attr( $key ); ?>]" class="htl-ui-input htl-ui-input--checkbox htl-ui-input--card-icon" type="checkbox" value="1" <?php echo checked( 1, $enabled, false ); ?> />
-
-		<label class="htl-ui-label htl-ui-label--checkbox htl-ui-label--card-icon" for="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>][<?php echo esc_attr( $key ); ?>]">
-			<span class="htl-ui-icon htl-ui-icon--<?php echo esc_attr( $key ); ?>"><?php echo wp_kses_post( $option ); ?></span>
-		</label>
+			<label class="htl-ui-label htl-ui-label--checkbox htl-ui-label--card-icon" for="hotelier_settings[<?php echo esc_attr( $args[ 'id' ] ); ?>][<?php echo esc_attr( $key ); ?>]">
+				<span class="htl-ui-card-icon htl-ui-card-icon--<?php echo esc_attr( $key ); ?>"><?php echo wp_kses_post( $option ); ?></span>
+			</label>
+		</div>
 	<?php endforeach; ?>
 
 	<div class="htl-ui-setting__description htl-ui-setting__description--card-icons"><?php echo wp_kses_post( $args[ 'desc' ] ); ?></div>
