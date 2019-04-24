b948086e0317c77235bef0187b05685f652dcf62
Benito Lopez <hello@lopezb.com>
2019-04-24 17:00:35 +0200

    Add notice class to section description

--- includes/admin/settings/views/fields/html-settings-field-description.php
+++ includes/admin/settings/views/fields/html-settings-field-description.php
@@ -9,6 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<div class="htl-ui-setting htl-ui-setting--section-description htl-ui-setting--<?php echo esc_attr( $args[ 'id' ] ); ?>">
-	<p class="htl-ui-setting--section-description__text"><?php echo wp_kses_post( $args[ 'desc' ] ); ?></p>
+<div class="htl-ui-setting htl-ui-setting--section-description htl-ui-setting--<?php echo esc_attr( $args[ 'id' ] ); ?> htl-ui-notice htl-ui-notice--info">
+	<p class="htl-ui-setting--section-description__text htl-ui-notice__text htl-ui-notice--info__text"><?php echo wp_kses_post( $args[ 'desc' ] ); ?></p>
 </div>
