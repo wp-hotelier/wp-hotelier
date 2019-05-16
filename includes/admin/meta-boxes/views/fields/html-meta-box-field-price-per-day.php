351ddc6f412dc1bc8d5c3b75abd9106786438c19
Benito Lopez <hello@lopezb.com>
2019-05-16 17:39:07 +0200

    Add wrapper to price per day inputs

--- includes/admin/meta-boxes/views/fields/html-meta-box-field-price-per-day.php
+++ includes/admin/meta-boxes/views/fields/html-meta-box-field-price-per-day.php
@@ -37,21 +37,23 @@ $locale_start_of_week = get_option( 'start_of_week' );
 	</div>
 
 	<div class="htl-ui-layout__column htl-ui-layout__column--right">
-		<?php for ( $i = 0; $i < 7; $i ++ ) : ?>
-			<?php
-			$day_index   = ( $locale_start_of_week + $i ) % 7;
-			$day         = $wp_locale->get_weekday( $day_index );
-			$day_initial = $wp_locale->get_weekday_initial( $day );
-			$value       = is_array( $field_value ) && isset( $field_value[ $day_index ] ) ? HTL_Formatting_Helper::localized_amount( $field_value[ $day_index ] ) : '';
-			?>
+		<div class="htl-ui-price-per-day-wrapper">
+			<?php for ( $i = 0; $i < 7; $i ++ ) : ?>
+				<?php
+				$day_index   = ( $locale_start_of_week + $i ) % 7;
+				$day         = $wp_locale->get_weekday( $day_index );
+				$day_initial = $wp_locale->get_weekday_initial( $day );
+				$value       = is_array( $field_value ) && isset( $field_value[ $day_index ] ) ? HTL_Formatting_Helper::localized_amount( $field_value[ $day_index ] ) : '';
+				?>
 
-			<div class="htl-ui-price-day">
-				<label class="htl-ui-label htl-ui-price-day__label">
-					<span class="htl-ui-price-day__name"><?php echo esc_html( $day_initial ); ?></span>
-					<input type="text" class="<?php echo esc_attr( $field[ 'class' ] ); ?> htl-ui-input--small htl-ui-input htl-ui-input--text htl-ui-input--price htl-ui-price-day__input" name="<?php echo esc_attr( $field[ 'name' ] ); ?>[<?php echo absint( $day_index ); ?>]" value="<?php echo esc_attr( $value ); ?>" placeholder="<?php echo esc_attr( HTL_Meta_Box_Room_Settings::get_price_placeholder() ); ?>" />
-				</label>
-			</div>
-		<?php endfor; ?>
+				<div class="htl-ui-price-day">
+					<label class="htl-ui-label htl-ui-price-day__label">
+						<span class="htl-ui-price-day__name"><?php echo esc_html( $day_initial ); ?></span>
+						<input type="text" class="<?php echo esc_attr( $field[ 'class' ] ); ?> htl-ui-input--small htl-ui-input htl-ui-input--text htl-ui-input--price htl-ui-price-day__input" name="<?php echo esc_attr( $field[ 'name' ] ); ?>[<?php echo absint( $day_index ); ?>]" value="<?php echo esc_attr( $value ); ?>" placeholder="<?php echo esc_attr( HTL_Meta_Box_Room_Settings::get_price_placeholder() ); ?>" />
+					</label>
+				</div>
+			<?php endfor; ?>
+		</div>
 
 		<?php if ( ! empty( $field[ 'description' ] ) ) : ?>
 			<div class="htl-ui-setting__description htl-ui-setting__description--price-per-day"><?php echo wp_kses_post( $field[ 'description' ] ); ?></div>
