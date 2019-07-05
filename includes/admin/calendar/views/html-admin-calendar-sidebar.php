a861e5dd24607c24bb6f016f63ba3056d8958d32
Benito Lopez <hello@lopezb.com>
2019-07-05 18:53:41 +0200

    Pass data status and disable statuses by default

--- includes/admin/calendar/views/html-admin-calendar-sidebar.php
+++ includes/admin/calendar/views/html-admin-calendar-sidebar.php
@@ -13,10 +13,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<ul class="booking-calendar-filters__list">
 		<?php foreach ( htl_get_reservation_statuses() as $id => $name ) : ?>
 			<?php
-			$id = esc_attr( str_replace( 'htl-', '', $id ) );
+			$id         = esc_attr( str_replace( 'htl-', '', $id ) );
+			$not_active = in_array( $id, $default_disabled_statuses ) ? true : false;
 			?>
 
-			<li class="booking-calendar-filters__item booking-calendar-filters__item--<?php echo esc_attr( $id ); ?>">
+			<li class="booking-calendar-filters__item booking-calendar-filters__item--<?php echo esc_attr( $id ); ?> <?php echo $not_active ? 'not-active' : ''; ?>" data-status="<?php echo esc_attr( $id ); ?>">
 				<?php echo esc_html( $name ); ?>
 			</li>
 		<?php endforeach; ?>
