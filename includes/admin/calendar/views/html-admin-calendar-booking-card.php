6651611e503ce280446955804c4f41ef6e491251
Benito Lopez <hello@lopezb.com>
2019-07-10 15:35:14 +0200

    Fix reservation details on booking card

--- includes/admin/calendar/views/html-admin-calendar-booking-card.php
+++ includes/admin/calendar/views/html-admin-calendar-booking-card.php
@@ -7,12 +7,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-$_reservation_id       = $cell[ 'id' ];
-$_reservation_nights   = $duration;
-$_reservation_checkin  = $reservation[ 'checkin' ];
-$_reservation_checkout = $reservation[ 'checkout' ];
-$_reservation_status   = $reservation[ 'status' ];
-$_reservation_duration = $duration;
+$_reservation_id = $cell[ 'id' ];
+$_reservation    = htl_get_reservation( $_reservation_id );
 
 ?>
 
@@ -20,12 +16,12 @@ $_reservation_duration = $duration;
 
 	<h4 class="booking-calendar-card__title"><?php echo esc_html( get_the_title( $_reservation_id ) ) ?></h4>
 
-	<span class="booking-calendar-card__dates"><?php echo sprintf( esc_html__( 'Dates: %1$s - %2$s', 'wp-hotelier' ), $_reservation_checkin, $_reservation_checkout ); ?></span>
+	<span class="booking-calendar-card__dates"><?php echo sprintf( esc_html__( 'Dates: %1$s - %2$s', 'wp-hotelier' ), $_reservation->get_checkin(), $_reservation->get_checkout() ); ?></span>
 
 	<ul class="booking-calendar-card-details__list">
 		<li class="booking-calendar-card-details__item">
 			<strong class="booking-calendar-card-details__label"><?php esc_html_e( 'Number of nights:' ) ?></strong>
-			<span class="booking-calendar-card-details__text"><?php echo sprintf( esc_html__( '%d-night stay', 'wp-hotelier' ), $_reservation_duration ) ?></span>
+			<span class="booking-calendar-card-details__text"><?php echo sprintf( esc_html__( '%d-night stay', 'wp-hotelier' ), $_reservation->get_nights() ) ?></span>
 		</li>
 
 		<li class="booking-calendar-card-details__item">
@@ -34,7 +30,7 @@ $_reservation_duration = $duration;
 		</li>
 	</ul>
 
-	<span class="htl-ui-text-icon-button htl-ui-text-icon-button--left htl-ui-text-icon-button--status htl-ui-text-icon-button--status-<?php echo esc_html( $_reservation_status ); ?>"><?php echo esc_html( $_reservation_status ); ?></span>
+	<span class="htl-ui-text-icon-button htl-ui-text-icon-button--left htl-ui-text-icon-button--status htl-ui-text-icon-button--status-<?php echo esc_html( $_reservation->get_status() ); ?>"><?php echo esc_html( $_reservation->get_status() ); ?></span>
 
-	<a href="<?php echo esc_url( get_edit_post_link( $_reservation_id ) ); ?>"><?php esc_html_e( 'View reservation', 'wp-hotelier' ); ?></a>
+	<a class="htl-ui-icon booking-calendar-card-details__view-link" href="<?php echo esc_url( get_edit_post_link( $_reservation_id ) ); ?>"><?php esc_html_e( 'View reservation', 'wp-hotelier' ); ?></a>
 </div>
