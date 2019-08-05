eb6d6555b8e419dc42a7778347e85d75cbd8240e
Benito Lopez <hello@lopezb.com>
2019-08-05 09:40:41 +0200

    Introduce modals

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -36,6 +36,31 @@ jQuery(function ($) {
 		}
 	};
 
+	var HTL_Modals = {
+		init: function () {
+			$('.htl-ui-button--open-modal').on('click', function () {
+				var _this = $(this);
+				var modal = $('#' + _this.attr('data-open-modal'));
+				var obfuscator = document.createElement('div');
+
+				obfuscator.id = 'htl-ui-modal-obfuscator';
+				document.documentElement.appendChild(obfuscator);
+
+				obfuscator.addEventListener('click', HTL_Modals.close_modal);
+				obfuscator = $('#htl-ui-modal-obfuscator');
+
+				obfuscator.show();
+				modal.show();
+
+				modal.find('.htl-ui-modal__cancel').on('click', HTL_Modals.close_modal);
+			});
+		},
+
+		close_modal: function () {
+			$('.htl-ui-modal').hide();
+			$('#htl-ui-modal-obfuscator').remove();
+		}
+	};
 	$(document).ready(function () {
 		HTL_Admin.init();
 	});
