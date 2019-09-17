74a04799d4addfc8e8640a46f576fb63614ca9b9
Benito Lopez <hello@lopezb.com>
2019-09-17 14:56:46 +0200

    Bump version

--- includes/emails/class-htl-email-guest-invoice.php
+++ includes/emails/class-htl-email-guest-invoice.php
@@ -5,7 +5,7 @@
  * @author   Benito Lopez <hello@lopezb.com>
  * @category Class
  * @package  Hotelier/Classes/Emails
- * @version  1.0.0
+ * @version  2.0.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
