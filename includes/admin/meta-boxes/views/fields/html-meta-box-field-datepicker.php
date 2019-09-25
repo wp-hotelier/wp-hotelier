93f50d4372ae6024240211cc6c7f14b21839d23a
Benito Lopez <hello@lopezb.com>
2019-09-25 17:10:42 +0200

    Include new helper for datepicker inputs

---
+++ includes/admin/meta-boxes/views/fields/html-meta-box-field-datepicker.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Meta Box Field "Datepicker"
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $thepostid, $post;
+
+$thepostid                = empty( $thepostid ) ? $post->ID : $thepostid;
+$field[ 'wrapper_class' ] = isset( $field[ 'wrapper_class' ] ) ? $field[ 'wrapper_class' ] : '';
+$field[ 'class' ]         = isset( $field[ 'class' ] ) ? $field[ 'class' ] : '';
+$field[ 'name' ]          = isset( $field[ 'name' ] ) ? $field[ 'name' ] : $field[ 'id' ];
+$field[ 'label' ]         = isset( $field[ 'label' ] ) ? $field[ 'label' ] : '';
+$field[ 'placeholder' ]   = isset( $field[ 'placeholder' ] ) ? $field[ 'placeholder' ] : '';
+
+// Set field value
+if ( isset( $field[ 'value' ] ) && $field[ 'value' ] ) {
+	$field_value = $field[ 'value' ];
+} else if ( isset( $field[ 'std' ] ) ) {
+	// This means that if a std value is passed
+	// the input will never be empty
+	$field_value = $field[ 'std' ];
+} else {
+	$field_value = '';
+}
+
+?>
+
+<div class="htl-ui-setting htl-ui-setting--metabox htl-ui-setting--datepicker <?php echo esc_attr( $field[ 'wrapper_class' ] ); ?> htl-ui-layout htl-ui-layout--two-columns">
+
+	<div class="htl-ui-layout__column htl-ui-layout__column--left">
+		<h3 class="htl-ui-heading htl-ui-setting__title"><?php echo esc_html( $field[ 'label' ] ); ?></h3>
+
+		<?php if ( isset( $field[ 'after_label' ] ) ) : ?>
+			<div class="htl-ui-setting__title-description"><?php echo wp_kses_post( $field[ 'after_label' ] ); ?></div>
+		<?php endif; ?>
+	</div>
+
+	<div class="htl-ui-layout__column htl-ui-layout__column--right">
+		<span class="htl-ui-datepicker-wrapper">
+			<input class="htl-ui-input htl-ui-input--datepicker <?php echo esc_attr( $field[ 'class' ] ); ?>" type="text" placeholder="YYYY-MM-DD" name="<?php echo esc_attr( $field[ 'name' ] ); ?>" value="<?php echo esc_attr( $field_value ); ?>" autocomplete="off">
+		</span>
+
+		<?php if ( isset( $field[ 'after_input' ] ) ) : ?>
+			<span class="htl-ui-setting__after-input"><?php echo wp_kses_post( $field[ 'after_input' ] ); ?></span>
+		<?php endif; ?>
+
+		<?php if ( ! empty( $field[ 'description' ] ) ) : ?>
+			<div class="htl-ui-setting__description htl-ui-setting__description--datepicker"><?php echo wp_kses_post( $field[ 'description' ] ); ?></div>
+		<?php endif; ?>
+	</div>
+</div>
