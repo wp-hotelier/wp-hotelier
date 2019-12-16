893de4d6f4f3270fe5fd251af3166dda633ba304
jvoiture <mokharbi9@smail.teknotit.com>
2019-12-16 13:22:45 +0000

    Update class-htl-meta-box-reservation-data.php
    when guest_arrival_time = 0 which corresponds to "00:00 - 01:00",  correct value not selected in the metabox

--- includes/admin/meta-boxes/class-htl-meta-box-reservation-data.php
+++ includes/admin/meta-boxes/class-htl-meta-box-reservation-data.php
@@ -327,7 +327,7 @@ class HTL_Meta_Box_Reservation_Data {
 				HTL_Meta_Boxes_Helper::select_input(
 					array(
 						'name'    => 'guest_arrival_time',
-						'value'   => $reservation->get_arrival_time(),
+						'std'   => $reservation->get_arrival_time(),
 						'label'   => esc_html__( 'Estimated arrival time:', 'wp-hotelier' ),
 						'options' => array(
 							'-1' => esc_html__( 'I don\'t know', 'wp-hotelier' ),
