829ea80b66a0f98b19b558d950364e0e1a07ed5b
Benito Lopez <hello@lopezb.com>
2020-07-10 08:52:38 +0200

    New action before datepicker input

--- templates/global/datepicker.php
+++ templates/global/datepicker.php
@@ -24,6 +24,8 @@ $datepicker_form_url = apply_filters( 'hotelier_datepicker_form_url', HTL()->car
 
 <form name="hotelier_datepicker" method="post" id="hotelier-datepicker" class="datepicker-form" action="<?php echo esc_url( $datepicker_form_url ); ?>" enctype="multipart/form-data">
 
+	<?php do_action( 'hotelier_datepicker_before_input', $shortcode_atts ); ?>
+
 	<span class="datepicker-input-select-wrapper"><input class="datepicker-input-select" type="text" id="hotelier-datepicker-select" value=""></span>
 	<input type="text" id="hotelier-datepicker-checkin" class="datepicker-input datepicker-input--checkin" name="checkin" value="<?php echo esc_attr( $checkin ); ?>">
 	<input type="text" id="hotelier-datepicker-checkout" class="datepicker-input datepicker-input--checkout" name="checkout" value="<?php echo esc_attr( $checkout ); ?>">
