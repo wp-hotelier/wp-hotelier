ebb690d4ca307c26d78701f998b4fef0094d47d6
Benito Lopez <hello@lopezb.com>
2020-09-16 16:47:00 +0200

    Add the ability to filter the cancel reservation button classes

--- templates/reservation/cancel-reservation.php
+++ templates/reservation/cancel-reservation.php
@@ -13,8 +13,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+$cancel_button_classes = apply_filters( 'hotelier_cancel_reservation_button_classes', array( 'button', 'button--cancel-reservation-button' ) );
 ?>
 
 <p class="cancel-reservation">
-	<a href="<?php echo esc_url( $reservation->get_booking_cancel_url() ); ?>" class="button button--cancel-reservation-button"><?php _e( 'Cancel reservation', 'wp-hotelier' ); ?></a>
+	<a href="<?php echo esc_url( $reservation->get_booking_cancel_url() ); ?>" class="<?php echo esc_attr( implode( ' ', $cancel_button_classes ) ); ?>"><?php _e( 'Cancel reservation', 'wp-hotelier' ); ?></a>
 </p>
