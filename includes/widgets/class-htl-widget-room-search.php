24d636d3c9ce1af4671ea5390726374941e1f74d
Benito Lopez <hello@lopezb.com>
2020-10-07 15:54:09 +0200

    Use a template for the room search form

--- includes/widgets/class-htl-widget-room-search.php
+++ includes/widgets/class-htl-widget-room-search.php
@@ -45,20 +45,7 @@ class HTL_Widget_Room_Search extends HTL_Widget {
 	public function widget( $args, $instance ) {
 		$this->widget_start( $args, $instance );
 
-		do_action( 'hotelier_before_widget_room_search' );
-		?>
-
-		<form role="search" method="get" class="form--room-search room-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
-			<label class="screen-reader-text" for="s"><?php esc_html_e( 'Search for:', 'wp-hotelier' ); ?></label>
-			<input type="search" class="room-search__input" placeholder="<?php echo esc_attr_x( 'Search rooms&hellip;', 'placeholder', 'wp-hotelier' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'wp-hotelier' ); ?>" />
-			<input class="button button--room-search" type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'wp-hotelier' ); ?>" />
-			<input type="hidden" name="post_type" value="room" />
-
-			<?php do_action( 'hotelier_after_widget_room_search_fields' ); ?>
-		</form>
-
-		<?php
-		do_action( 'hotelier_after_widget_room_search' );
+		htl_get_template( 'widgets/room-searchform.php' );
 
 		$this->widget_end( $args );
 	}
---
+++ templates/widgets/room-searchform.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying room widget search
+ *
+ * This template can be overridden by copying it to yourtheme/hotelier/widgets/room-searchform.php.
+ *
+ * @author  Benito Lopez <hello@lopezb.com>
+ * @package Hotelier/Templates
+ * @version 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+
+<?php do_action( 'hotelier_before_widget_room_search' ); ?>
+
+<form role="search" method="get" class="form--room-search room-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
+	<label class="screen-reader-text" for="s"><?php esc_html_e( 'Search for:', 'wp-hotelier' ); ?></label>
+	<input type="search" class="room-search__input" placeholder="<?php echo esc_attr_x( 'Search rooms&hellip;', 'placeholder', 'wp-hotelier' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'wp-hotelier' ); ?>" />
+	<input class="button button--room-search" type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'wp-hotelier' ); ?>" />
+	<input type="hidden" name="post_type" value="room" />
+
+	<?php do_action( 'hotelier_after_widget_room_search_fields' ); ?>
+</form>
+
+<?php do_action( 'hotelier_after_widget_room_search' );
