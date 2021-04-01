0c6d46af4ac70653843819529f3f195ed479b183
Benito Lopez <hello@lopezb.com>
2021-04-01 15:27:01 +0200

    Calculate discount to apply

--- includes/htl-coupon-functions.php
+++ includes/htl-coupon-functions.php
@@ -30,3 +30,32 @@ function htl_get_coupon( $the_coupon = false ) {
 function htl_coupons_enabled() {
 	return apply_filters( 'hotelier_coupons_enabled', htl_get_option( 'enable_coupons' ) );
 }
+
+/**
+ * Calculate coupon.
+ *
+ * @param  int $amount Amount without coupon.
+ * @return int
+ */
+function htl_calculate_coupon( $amount, $coupon_id ) {
+	$coupons_enabled = htl_coupons_enabled();
+
+	// Return early if coupons are not enabled
+	if ( ! $coupons_enabled ) {
+		return 0;
+	}
+
+	$coupon = htl_get_coupon( $coupon_id );
+
+	if ( $coupon->get_type() === 'fixed' ) {
+		$amount_to_reduce = $coupon->get_amount();
+	} else {
+		$percentage_to_reduce = $coupon->get_amount();
+		$amount_to_reduce     = ( $amount * $percentage_to_reduce ) / 100;
+	}
+
+	$calculated_coupon = ceil( $amount_to_reduce );
+	$calculated_coupon = apply_filters( 'hotelier_calulate_coupon', $calculated_coupon, $amount, $coupon_id );
+
+	return absint( $calculated_coupon );
+}
