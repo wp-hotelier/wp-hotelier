91c01e9573e3e2899d2500a2a111e453cbd0018e
Benito Lopez <hello@lopezb.com>
2021-09-29 14:38:59 +0200

    Bump versions and changelog

--- hotelier.php
+++ hotelier.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WP Hotelier
  * Plugin URI:        https://wphotelier.com/?utm_source=wpadmin&utm_medium=plugin&utm_campaign=wphotelierplugin
  * Description:       Hotel booking plugin for WordPress.
- * Version:           2.6.1
+ * Version:           2.6.2
  * Author:            WP Hotelier
  * Author URI:        https://wphotelier.com/
  * Requires at least: 4.0
@@ -32,7 +32,7 @@ final class Hotelier {
 	/**
 	 * @var string
 	 */
-	public $version = '2.6.1';
+	public $version = '2.6.2';
 
 	/**
 	 * @var Hotelier The single instance of the class
