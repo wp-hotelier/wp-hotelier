b0c3163423a07afad7963be994b8b3aae6f5d233
Benito Lopez <hello@lopezb.com>
2022-02-03 18:28:32 +0100

    Add new actions before/after room deposit

--- templates/single-room/deposit.php
+++ templates/single-room/deposit.php
@@ -17,9 +17,13 @@ global $room;
 
 if ( ! $room->is_variable_room() && $room->needs_deposit() ) : ?>
 
+<?php do_action( 'hotelier_before_single_room_deposit' ); ?>
+
 <div class="room__deposit room__deposit--single">
 	<span class="room__deposit-label room__deposit-label--single"><?php esc_html_e( 'Deposit required', 'wp-hotelier' ); ?></span>
 	<span class="room__deposit-amount room__deposit-amount--single"><?php echo wp_kses( $room->get_formatted_deposit(), array( 'span' => array( 'class' => array() ) ) ); ?></span>
 </div>
+
+<?php do_action( 'hotelier_after_single_room_deposit' ); ?>
 
 <?php endif; ?>
