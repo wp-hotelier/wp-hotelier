2a98d595d1e7e33907a071d89b63dd3eac5c6e8a
Benito Lopez <hello@lopezb.com>
2022-06-23 22:48:43 +0200

    Allow to filter the room catalog size via hook

--- templates/archive/content/room-image.php
+++ templates/archive/content/room-image.php
@@ -6,7 +6,7 @@
  *
  * @author  Benito Lopez <hello@lopezb.com>
  * @package Hotelier/Templates
- * @version 1.0.0
+ * @version 2.9.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -17,8 +17,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <a href="<?php the_permalink() ?>" class="room__thumbnail room__thumbnail--loop">
 	<?php if ( has_post_thumbnail() ) :
-		the_post_thumbnail( 'room_catalog' );
+		the_post_thumbnail( apply_filters( 'hotelier_loop_room_image_size', 'room_catalog' ) );
 	else :
-		echo htl_placeholder_img( 'room_catalog' );
+		echo htl_placeholder_img( apply_filters( 'hotelier_loop_room_image_size', 'room_catalog' ) );
 	endif; ?>
 </a>
