e89cd66a965ddaaa43069ac39dba8abab4376157
Benito Lopez <hello@lopezb.com>
2022-08-12 18:20:07 +0200

    Update hotel-datepicker library

--- includes/class-htl-frontend-scripts.php
+++ includes/class-htl-frontend-scripts.php
@@ -162,7 +162,7 @@ class HTL_Frontend_Scripts {
 
 		// Localize and enqueue the datepicker scripts
 		wp_register_script( 'fecha', HTL_PLUGIN_URL . 'assets/js/lib/fecha/fecha.min.js', array(), '4.2.1', true );
-		wp_register_script( 'hotel-datepicker', HTL_PLUGIN_URL . 'assets/js/lib/hotel-datepicker/hotel-datepicker.min.js', array( 'fecha' ), '4.3.0', true );
+		wp_register_script( 'hotel-datepicker', HTL_PLUGIN_URL . 'assets/js/lib/hotel-datepicker/hotel-datepicker.min.js', array( 'fecha' ), '4.4.0', true );
 
 		wp_register_script( 'hotelier-init-datepicker', HTL_PLUGIN_URL . 'assets/js/frontend/hotelier-init-datepicker' . $suffix . '.js', array( 'jquery', 'hotel-datepicker' ), HTL_VERSION, true );
 
