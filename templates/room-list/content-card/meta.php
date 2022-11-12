8a9400348baf53cf15758ae17d4b4d89007413ca
Benito Lopez <hello@lopezb.com>
2022-11-12 22:19:11 +0100

    Show bathrooms in listing page (card)

--- templates/room-list/content-card/meta.php
+++ templates/room-list/content-card/meta.php
@@ -19,12 +19,16 @@ global $room;
 
 <div class="room-card__meta">
 
-	<?php if ( $room->get_room_size() ) : ?>
-		<span class="room-card__size"><?php echo esc_html( $room->get_formatted_room_size() ); ?></span>
+	<?php if ( $bed_size = $room->get_bed_size() ) : ?>
+		<span class="room-card__beds"><?php echo esc_html( $bed_size ); ?></span>
 	<?php endif; ?>
 
-	<?php if ( $room->get_bed_size() ) : ?>
-		<span class="room-card__beds"><?php echo esc_html( $room->get_bed_size() ); ?></span>
+	<?php if ( $bathrooms = $room->get_bathrooms() ) : ?>
+		<span class="room-card__bathrooms"><?php echo esc_html( sprintf( _n( '%s bathroom', '%s bathrooms', $bathrooms, 'wp-hotelier' ), absint( $bathrooms ) ) ); ?></span>
+	<?php endif; ?>
+
+	<?php if ( $room->get_room_size() ) : ?>
+		<span class="room-card__size"><?php echo esc_html( $room->get_formatted_room_size() ); ?></span>
 	<?php endif; ?>
 
 </div>
