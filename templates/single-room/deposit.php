79c18a71a2456b6f1b49035ca4c68410aa08a34b
Benito Lopez <hello@lopezb.com>
2023-01-14 09:21:53 +0100

    Add the ability to use the long formatted deposit in single room

--- templates/single-room/deposit.php
+++ templates/single-room/deposit.php
@@ -19,10 +19,16 @@ if ( ! $room->is_variable_room() && $room->needs_deposit() ) : ?>
 
 <?php do_action( 'hotelier_before_single_room_deposit' ); ?>
 
-<div class="room__deposit room__deposit--single">
-	<span class="room__deposit-label room__deposit-label--single"><?php esc_html_e( 'Deposit required', 'wp-hotelier' ); ?></span>
-	<span class="room__deposit-amount room__deposit-amount--single"><?php echo wp_kses( $room->get_formatted_deposit(), array( 'span' => array( 'class' => array() ) ) ); ?></span>
-</div>
+<?php if ( apply_filters( 'hotelier_single_room_long_formatted_deposit', false ) ) : ?>
+	<div class="room__deposit room__deposit--single">
+		<?php echo wp_kses_post( $room->get_long_formatted_deposit() ); ?>
+	</div>
+<?php else:  ?>
+	<div class="room__deposit room__deposit--single">
+		<span class="room__deposit-label room__deposit-label--single"><?php esc_html_e( 'Deposit required', 'wp-hotelier' ); ?></span>
+		<span class="room__deposit-amount room__deposit-amount--single"><?php echo wp_kses( $room->get_formatted_deposit(), array( 'span' => array( 'class' => array() ) ) ); ?></span>
+	</div>
+<?php endif; ?>
 
 <?php do_action( 'hotelier_after_single_room_deposit' ); ?>
 
