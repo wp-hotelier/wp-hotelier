be5a152b985cb4b3b199d036364d9a42a8bbc52c
Benito Lopez <hello@lopezb.com>
2023-01-18 17:03:25 +0100

    Add the ability to show the longe formatted deposit via hook

--- templates/single-room/rate/rate-deposit.php
+++ templates/single-room/rate/rate-deposit.php
@@ -6,7 +6,7 @@
  *
  * @author  Benito Lopez <hello@lopezb.com>
  * @package Hotelier/Templates
- * @version 1.2.0
+ * @version 2.9.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -16,8 +16,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( $variation->needs_deposit() ) : ?>
 
 <div class="rate__deposit rate__deposit--single">
-	<span class="rate__deposit-label rate__deposit-label--single"><?php esc_html_e( 'Deposit required', 'wp-hotelier' ); ?></span>
-	<span class="rate__deposit-amount rate__deposit-amount--single"><?php echo wp_kses( $variation->get_formatted_deposit(), array( 'span' => array( 'class' => array() ) ) ); ?></span>
+	<?php if ( apply_filters( 'hotelier_single_room_long_formatted_deposit', false ) ) : ?>
+		<?php echo wp_kses_post( $variation->get_long_formatted_deposit() ); ?>
+	<?php else:  ?>
+		<span class="rate__deposit-label rate__deposit-label--single"><?php esc_html_e( 'Deposit required', 'wp-hotelier' ); ?></span>
+		<span class="rate__deposit-amount rate__deposit-amount--single"><?php echo wp_kses( $variation->get_formatted_deposit(), array( 'span' => array( 'class' => array() ) ) ); ?></span>
+	<?php endif; ?>
+
 </div>
 
 <?php endif; ?>
