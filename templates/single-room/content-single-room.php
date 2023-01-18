ea3a12df5dc5e610a086047e2cee490f0b807ffb
Benito Lopez <hello@lopezb.com>
2023-01-18 16:30:17 +0100

    Add the ability not to print default hooks

--- templates/single-room/content-single-room.php
+++ templates/single-room/content-single-room.php
@@ -33,84 +33,92 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<<?php echo esc_attr( apply_filters( 'hotelier_single_room_wrapper_tag', 'div' ) ); ?> id="room-<?php echo absint( get_the_ID() ); ?>" <?php post_class(); ?>>
 <?php endif; ?>
 
-	<?php
-		/**
-		 * hotelier_single_room_title hook.
-		 *
-		 * @hooked hotelier_template_single_room_title - 10
-		 */
-		do_action( 'hotelier_single_room_title' );
-	?>
+	<?php if ( apply_filters( 'hotelier_single_room_print_hooks', true ) ) : ?>
 
-	<?php do_action( 'hotelier_single_room_before_content' ); ?>
+		<?php
+			/**
+			 * hotelier_single_room_title hook.
+			 *
+			 * @hooked hotelier_template_single_room_title - 10
+			 */
+			do_action( 'hotelier_single_room_title' );
+		?>
 
-	<?php
-		/**
-		 * hotelier_single_room_images hook.
-		 *
-		 * @hooked hotelier_template_single_room_image - 10
-		 * @hooked hotelier_template_single_room_gallery - 20
-		 */
-		do_action( 'hotelier_single_room_images' );
-	?>
+		<?php do_action( 'hotelier_single_room_before_content' ); ?>
 
-	<div class="entry-content room__content room__content--single">
+		<?php
+			/**
+			 * hotelier_single_room_images hook.
+			 *
+			 * @hooked hotelier_template_single_room_image - 10
+			 * @hooked hotelier_template_single_room_gallery - 20
+			 */
+			do_action( 'hotelier_single_room_images' );
+		?>
 
-		<div class="room__details room__details--single">
+		<div class="entry-content room__content room__content--single">
+
+			<div class="room__details room__details--single">
+
+				<?php
+					/**
+					 * hotelier_single_room_details hook.
+					 *
+					 * @hooked hotelier_template_single_room_datepicker - 5
+					 * @hooked hotelier_template_single_room_price - 10
+					 * @hooked hotelier_template_single_room_non_cancellable_info - 15
+					 * @hooked hotelier_template_single_room_deposit - 20
+					 * @hooked hotelier_template_single_room_min_max_info - 25
+					 * @hooked hotelier_template_single_room_meta - 30
+					 * @hooked hotelier_template_single_room_facilities - 40
+					 * @hooked hotelier_template_single_room_conditions - 50
+					 * @hooked hotelier_template_single_room_sharing - 60
+					 */
+					do_action( 'hotelier_single_room_details' );
+				?>
+
+			</div>
+
+			<div class="room__description room__description--single">
+
+				<?php
+					/**
+					 * hotelier_single_room_description hook.
+					 *
+					 * @hooked hotelier_template_single_room_description - 10
+					 */
+					do_action( 'hotelier_single_room_description' );
+				?>
+
+			</div>
 
 			<?php
 				/**
-				 * hotelier_single_room_details hook.
+				 * hotelier_single_room_rates hook.
 				 *
-				 * @hooked hotelier_template_single_room_datepicker - 5
-				 * @hooked hotelier_template_single_room_price - 10
-				 * @hooked hotelier_template_single_room_non_cancellable_info - 15
-				 * @hooked hotelier_template_single_room_deposit - 20
-				 * @hooked hotelier_template_single_room_min_max_info - 25
-				 * @hooked hotelier_template_single_room_meta - 30
-				 * @hooked hotelier_template_single_room_facilities - 40
-				 * @hooked hotelier_template_single_room_conditions - 50
-				 * @hooked hotelier_template_single_room_sharing - 60
+				 * @hooked hotelier_template_single_room_rates - 10
 				 */
-				do_action( 'hotelier_single_room_details' );
+				do_action( 'hotelier_single_room_rates' );
+			?>
+
+			<?php
+				/**
+				 * hotelier_output_related_rooms hook.
+				 *
+				 * @hooked hotelier_template_related_rooms - 10
+				 */
+				do_action( 'hotelier_output_related_rooms' );
 			?>
 
 		</div>
 
-		<div class="room__description room__description--single">
+		<?php do_action( 'hotelier_single_room_after_content' ); ?>
 
-			<?php
-				/**
-				 * hotelier_single_room_description hook.
-				 *
-				 * @hooked hotelier_template_single_room_description - 10
-				 */
-				do_action( 'hotelier_single_room_description' );
-			?>
+	<?php else : ?>
 
-		</div>
+		<?php the_content(); ?>
 
-		<?php
-			/**
-			 * hotelier_single_room_rates hook.
-			 *
-			 * @hooked hotelier_template_single_room_rates - 10
-			 */
-			do_action( 'hotelier_single_room_rates' );
-		?>
-
-		<?php
-			/**
-			 * hotelier_output_related_rooms hook.
-			 *
-			 * @hooked hotelier_template_related_rooms - 10
-			 */
-			do_action( 'hotelier_output_related_rooms' );
-		?>
-
-	</div>
-
-	<?php do_action( 'hotelier_single_room_after_content' ); ?>
+	<?php endif; ?>
 
 <?php if ( apply_filters( 'hotelier_print_single_room_wrapper', true ) ) : ?>
 	</<?php echo esc_attr( apply_filters( 'hotelier_single_room_wrapper_tag', 'div' ) ); ?>>
