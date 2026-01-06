77fe89a2ead986096f55394b4db10f19174eda09
DAnn2012 <dannunziata@hotmail.it>
2026-01-06 16:55:35 +0100

    Update terms.php

--- templates/booking/terms.php
+++ templates/booking/terms.php
@@ -19,7 +19,7 @@ if ( htl_get_page_id( 'terms' ) > 0 ) : ?>
 	<p class="form-row form-row--booking-terms">
 		<input type="checkbox" class="input-checkbox input--booking-terms" name="booking_terms" <?php checked( isset( $_POST[ 'booking_terms' ] ), true ); ?> id="booking-terms" />
 
-		<label for="booking-terms" class="checkbox label--booking-terms"><?php printf( wp_kses_post( sprintf( __( 'I&rsquo;ve read and accept the <a href="%s" target="_blank">terms &amp; conditions</a>', 'wp-hotelier' ), esc_url( htl_get_page_permalink( 'terms' ) ) ) ) ); ?> <abbr class="required" title="<?php echo esc_attr__( 'required', 'wp-hotelier' ); ?>">*</abbr></label>
+		<label for="booking-terms" class="checkbox label--booking-terms"><?php echo wp_kses_post( sprintf( __( 'I&rsquo;ve read and accept the <a href="%s" target="_blank">terms &amp; conditions</a>', 'wp-hotelier' ), esc_url( htl_get_page_permalink( 'terms' ) ) ) ); ?> <abbr class="required" title="<?php echo esc_attr__( 'required', 'wp-hotelier' ); ?>">*</abbr></label>
 
 		<input type="hidden" name="has_terms_field" value="1" />
 	</p>
